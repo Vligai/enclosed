ef64cfdfd9a69043368c9d07f5e8e5be54ba0cb1
harden71 <gi.kapoglis@gmail.com>
2015-11-10 18:08:23 -0500

    Update server.c
    connected database to server but I did it with copy and paste :P. Vlad please try the code. Also in this version you have to already have in the folder that you are Data.db file

--- Beta/src/server.c
+++ Beta/src/server.c
@@ -3,9 +3,158 @@
   uses regular sockets during alpha, will use SSL in Beta version
 */
 #define _SERVER_H_
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
 #include "enclosed.h"
 char nick[MAX];
 char password[MAX];
+
+//Database_function
+struct Users {
+        int id;
+        int set;
+        char username[MAX_DATA];
+        char password[MAX_DATA];
+};
+
+struct Database {
+        struct Users rows[MAX_ROWS];
+};
+
+struct Connection {
+        FILE *file;
+        struct Database *db;
+};
+
+void die(const char *message)
+{
+        if(errno) {
+                perror(message);
+        } else {
+                printf("ERROR: %s\n", message);
+        }
+
+        exit(1);
+}
+void Users_print(struct Users *user)
+{
+        printf("%d %s %s\n",
+                user->id, user->username, user->password);
+}
+void Database_load(struct Connection *conn)
+{
+        int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
+        if(rc != 1) die("Failed to load database.");
+}
+
+struct Connection *Database_open(const char *filename, char mode)
+{
+        struct Connection *conn = malloc(sizeof(struct Connection));
+        if(!conn) die("Memory error");
+
+        conn->db = malloc(sizeof(struct Database));
+        if(!conn->db) die("Memory error");
+
+        if(mode == 'c') {
+                conn->file = fopen(filename, "w");
+        }else {
+                conn->file = fopen(filename, "r+");
+
+                if(conn->file) {
+                        Database_load(conn);
+                }
+        }
+
+        if(!conn-> file) die("Failed to open the file");
+
+        return conn;
+}
+void Database_close(struct Connection *conn)
+{
+        if(conn) {
+                if(conn->file) fclose(conn->file);
+                if(conn->db) free(conn->db);
+                free(conn);
+        }
+}
+void Database_write(struct Connection *conn)
+{
+        rewind(conn->file);
+
+        int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
+        if(rc != 1) die("failed to write database.");
+
+        rc = fflush(conn->file);
+        if(rc == -1) die("Cannot flush database.");
+}
+
+void Database_create(struct Connection *conn)
+{
+        int i = 0;
+
+        for(i = 0; i < MAX_ROWS; i++) {
+                //Initizalize Database
+                struct Users user = {.id = i, .set = 0};
+                // Assign database
+                conn->db->rows[i] = user;
+        }
+}
+
+void Database_set(struct Connection *conn, int id, const char *username, const char *password)
+{
+        struct Users *user = &conn->db->rows[id];
+        if(user->set) die("Already set, delete it first");
+
+        user->set = 1;
+        //we have to find a better way to do this rather than strncpy
+        char *res = strncpy(user->username, username, MAX_DATA);
+        if(!res) die("Username copy failed");
+
+        res = strncpy(user->password, password, MAX_DATA);
+        if(!res) die("Password copy failed");
+}
+void Database_get(struct Connection *conn, int id)
+{
+        struct Users *user = &conn->db->rows[id];
+
+        if(user->set) {
+                Users_print(user);
+        } else {
+                die("ID is not set");
+        }
+}
+
+void Database_delete(struct Connection *conn, int id)
+{
+        struct Users user = {.id = id, .set = 0};
+        conn->db->rows[id] = user;
+}
+
+void Database_list(struct Connection *conn)
+{
+        int i = 0;
+        struct Database *db = conn->db;
+
+        for(i = 0; i < MAX_ROWS; i++) {
+                struct Users *cur = &db->rows[i];
+
+                if(cur->set) {
+                        Users_print(cur);
+                }
+        }
+}
+
+
+
+
+
 void sfault1(int sig)
 {
   if(sig == SIGINT)
@@ -63,6 +212,11 @@ int main(int argc, char** argv)
   char buff[MAX];
   char command[MAX];
   signal(SIGINT, sfault1);
+  //database stuff
+char *filename = "Data.db";
+char action;
+struct Connection *conn = Database_open(filename, action);
+int id = 0;
   /*check numer of arguments when starting up server*/
   if(argc < 2)
     {
@@ -137,6 +291,8 @@ int main(int argc, char** argv)
 	      //md5_hash(password, md5_pass);
 	      puts(password);
 	      write(sockfd2, "~", 1);
+	      Database_set(conn, id, nick, password);
+              Database_write(conn);
 	    }
 	  /*nick - username user is going to log in with
 	   check for username in he name of data fiels*/
